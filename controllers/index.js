223ce71f4c7c75238fd5d52f39c85d73d4eae64a
Christopher Zenner <christopher.zenner@gmail.com>
2020-08-22 17:14:17 -0700

    feat(controllers): add projects controller
    The projects controller has a single function, which is to retrieve all projects from the database, sort them based on starred status and completion date, and return them to the API. Additional methods may be added as necessary in the future.

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = (() => {
+  return {
+    controller: require('./projects-controller')
+  }
+})()
---
+++ controllers/projects-controller.js
@@ -0,0 +1,11 @@
+const db = require('../models')
+
+module.exports = {
+  findAll: (req, res) => {
+    db.Project
+      .find(req.query)
+      .sort({ starred: 1, completionDate: 'desc' })
+      .then(docs => res.json(docs))
+      .catch(err => res.status(422).json(err.stack))
+  }
+}
