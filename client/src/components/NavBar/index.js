c8c77c6f3ba2e6d8f70a0bf1bd7c6c403e9d4a30
Christopher Zenner <christopher.zenner@gmail.com>
2020-08-27 19:57:07 -0700

    feat(components): add navbar components
    The NavBar component encapsulates the site's primary navigation.

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact-me">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#">Blog</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,6 @@
 export { default as Header } from './Header'
 export { default as Main } from './Main'
 export { default as Footer } from './Footer'
+
+// secondary components
+export { default as NavBar } from './NavBar'
