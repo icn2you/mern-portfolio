d276d56552abfeaf8df93f722f072cd99787810d
Christopher Zenner <christopher.zenner@gmail.com>
2020-08-28 19:42:36 -0700

    feat(components): complete header structure
    Complete structure of Header component, including navigation, personal introduction, and personal avatar.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { 
+  Intro, NavBar, Portrait 
+} from '../../components'
 
 const Header = () => {
   return (
-    <header>
-
+    <header className="angled-container">
+      <Container>
+        <Row>
+          <Col lg={4}>
+            {/* Personal Logo */}
+          </Col>
+          <Col lg={6}>
+            <NavBar />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={8}>
+            <Intro />
+          </Col>
+          <Col lg={4}>
+            <Portrait />
+          </Col>
+        </Row>
+      </Container>
     </header>
   )
 }
