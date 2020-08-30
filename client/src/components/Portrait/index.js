b99b4aa526e37acdd3f30e16b00e320b5c93dba2
Christopher Zenner <christopher.zenner@gmail.com>
2020-08-29 18:12:07 -0700

    refactor(assets): rename `img/` to `images/`

--- client/src/components/Portrait/index.js
+++ client/src/components/Portrait/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Portrait = () => {
   return (
     <img 
-      src={require('../../assets/img/avatar-me-2020.png')} 
+      src={require('../../assets/images/avatar-me-2020.png')} 
       alt="Christopher Zenner" 
       className="img-fluid" 
     />
