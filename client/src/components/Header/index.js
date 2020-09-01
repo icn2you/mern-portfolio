7889fe2fc61adb7f8501156ed640bcaac81d1529
Christopher Zenner <christopher.zenner@gmail.com>
2020-08-31 21:17:33 -0700

    refactor(components): add header comments
    - Add header comment to `style.scss`.
    - Add header comment to `index.js`.
    - Tweak Bootstrap layout.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,26 @@
+/***********************************************************************
+FSWD:  Christopher B. Zenner
+Date:  08/26/2020
+File:  index.js
+Ver.:  0.1.0 20200826
+
+This script contains the Header React component of my developer 
+portfolio.
+***********************************************************************/
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import { 
-  Intro, NavBar, Portrait 
-} from '../../components'
+import { Intro, NavBar, Portrait } from '../../components'
 import './style.scss'
 
 const Header = () => {
   return (
     <header className="skewed-container">
       <Container>
-        <Row>
-          <Col lg={4}>
+        <Row className="justify-content-between">
+          <Col lg={4} className="text-center text-lg-left">
             {/* Personal Logo */}
           </Col>
-          <Col lg={6}>
+          <Col lg={8} className="text-center text-lg-right">
             <NavBar />
           </Col>
         </Row>
