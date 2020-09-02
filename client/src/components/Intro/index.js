083f8c0cb1e9870cf2ad8faa668634329ecd9c53
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-01 19:21:37 -0700

    style(components): add header comment
    - Add header comment and import SCSS stylesheet.

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -1,4 +1,14 @@
+/***********************************************************************
+FSWD:  Christopher B. Zenner
+Date:  08/28/2020
+File:  index.js
+Ver.:  0.1.0 20200828
+
+This script contains the Intro React component of my developer 
+portfolio.
+***********************************************************************/
 import React from 'react'
+import './style.scss'
 
 const Intro = () => {
   return (
