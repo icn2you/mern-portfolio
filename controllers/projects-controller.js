7f6c2c3fb3304dfb01069c460eaf66a65d1b4426
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-09 20:34:47 -0700

    refactor(controllers): tweak query results sort
    Query results are sorted as follows:
    1. `starred`
    2. `projectType` in descending order, placing individual projects before group projects
    3. `completionDate` in descending order, placing the most recent project first

--- controllers/projects-controller.js
+++ controllers/projects-controller.js
@@ -4,7 +4,7 @@ module.exports = {
   findAll: (req, res) => {
     db.Project
       .find(req.query)
-      .sort({ starred: 1, completionDate: 'desc' })
+      .sort({ starred: 1, projectType: 'desc', completionDate: 'desc' })
       .then(docs => res.json(docs))
       .catch(err => res.status(422).json(err.stack))
   }
