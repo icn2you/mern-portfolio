5da7c69ed22ca71678f1440c276a23925cf0dd0d
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-11 17:56:36 -0700

    refactor(components): convert buttons to inputs
    - Convert Button components to `input` elements, rather than `button`
      elements.
    - Encapsulate Button components in a Flexbox container.
    - Set placeholder text for `textarea` element.

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap'
+import './style.scss'
 
 const ContactForm = () => {
   return (
@@ -8,7 +9,7 @@ const ContactForm = () => {
         <Form.Label>Name</Form.Label>
         <Form.Control 
           type="text" 
-          placeholder="Wonder Woman" 
+          placeholder="Wonder Woman"
         />
       </Form.Group>
       <Form.Group controlId="formContactMeEmail">
@@ -20,15 +21,28 @@ const ContactForm = () => {
       </Form.Group>
       <Form.Group controlId="formContactMeMsg">
         <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows="10" />
+        <Form.Control 
+          as="textarea" 
+          rows="10" 
+          placeholder="Talk to me!"
+        />
       </Form.Group>
-      <Form.Group controlId="formContactMeBtns">
-        <Button type="submit" className="btn btn-outline-submit">
-          Send
-        </Button>
-        <Button type="reset" className="btn btn-outline-reset">
-          Reset
-        </Button>
+      <Form.Group 
+        controlId="formContactMeBtns" 
+        className="d-flex justify-content-center"
+      >
+        <Button 
+          as="input" 
+          type="submit"
+          variant="outline-submit"
+          value="Send"
+        />
+        <Button
+          as="input" 
+          type="reset"
+          variant="outline-reset"
+          value="Reset"
+        />
       </Form.Group>
     </Form>
   )
