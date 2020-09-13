d692056267c07a71b40ea82375fc2644245bbf0b
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-12 18:55:25 -0700

    feat(components): add copyright
    - Add Copyright component, which renders app copyright.
    - Export new component via `index.js` (to be commited subsequently).

---
+++ client/src/components/Copyright/index.js
@@ -0,0 +1,21 @@
+/***********************************************************************
+FSWD:  Christopher B. Zenner
+Date:  09/12/2020
+File:  index.js
+Ver.:  0.1.0 20200912
+
+This script contains the Copyright React component of my developer 
+portfolio.
+***********************************************************************/
+import React from 'react'
+import './style.scss'
+
+const Copyright = () => {
+  return (
+    <div id="copyright" className="text-center">
+      &copy; 2020 Christopher B. Zenner. All rights reserved.
+    </div>
+  )
+}
+
+export default Copyright
