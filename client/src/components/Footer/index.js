7d0b295e1e962ccce2e523dd34a6b8c2308eca98
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-13 18:02:43 -0700

    feat(components): add footer
    - Add Footer component, which renders footer content, including:
      - biosketch
      - inspirational quote
      - links to email and résumé as well as social media profiles
    - Add `index.js`, exporting all footer components.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,11 +8,54 @@ This script contains the Footer React component of my developer
 portfolio.
 ***********************************************************************/
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { Biosketch, Copyright, Quote, SocialMedia } from '../'
+import './style.scss'
+
+const socialMediaProfiles = [
+  { label: 'Email', 
+    icon: 'envelope', 
+    link: 'mailto:christopher.zenner@differentdrummer.dev?subject=Message&nbsp;from&nbsp;a&nbsp;Visitor&nbsp;to&nbsp;Your&nbsp;Dev&nbsp;Portfolio' },
+  { label: 'Résumé', 
+    icon: 'file-word', 
+    link: 'https://drive.google.com/file/d/16f1ij1jwi_jEuPEcyCHgfz3A2673DshP/view?usp=sharing' },
+  { label: 'GitHub', 
+    icon: 'github', 
+    link: 'https://github.com/icn2you' },
+  { label: 'LinkedIn', 
+    icon: 'linkedin-in', 
+    link: 'https://www.linkedin.com/in/icn2you/' },
+  { label: 'Twitter', 
+    icon: 'twitter',
+    link: 'https://twitter.com/icn2you' }
+];
 
 const Footer = () => {
   return (
     <footer>
-
+      <div id="about-me-container" class="skewed-container">
+        <Container>
+          <Row className="justify-content-between">
+            <Col lg={6} className="align-self-start">
+              <Biosketch />
+              <Quote />
+            </Col>
+            <Col lg={4} className="align-self-center">
+            { socialMediaProfiles
+              ? socialMediaProfiles.map(profile =>
+                <SocialMedia 
+                  label={profile.label}
+                  icon={profile.icon}
+                  link={profile.link}
+                />
+              )
+              : ''
+            }
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Copyright />
     </footer>
   )
 }
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,8 +4,12 @@ export { default as Main } from './Main'
 export { default as Footer } from './Footer'
 
 // secondary components
+export { default as Biosketch } from './Biosketch'
 export { default as ContactForm } from './ContactForm'
+export { default as Copyright } from './Copyright'
 export { default as Intro } from './Intro'
 export { default as NavBar } from './NavBar'
 export { default as Portrait } from './Portrait'
 export { default as Project } from './Project'
+export { default as Quote } from './Quote'
+export { default as SocialMedia } from './SocialMedia'
