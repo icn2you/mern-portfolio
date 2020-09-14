8727b9af5ab51a1eca9c8a79c651d1f4ae68e3a5
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-13 17:47:29 -0700

    feat(components): add quote
    - Add Quote component, which renders inspirational quote.
    - Export new component via `index.js` (to be commited subsequently).

---
+++ client/src/components/Quote/index.js
@@ -0,0 +1,23 @@
+/***********************************************************************
+FSWD:  Christopher B. Zenner
+Date:  09/12/2020
+File:  index.js
+Ver.:  0.1.0 20200912
+
+This script contains the Quote React component of my developer 
+portfolio.
+***********************************************************************/
+import React from 'react'
+import './style.scss'
+
+const Quote = () => {
+  return (
+    <div>
+      <blockquote cite="Howard Thurman">
+        Don't ask yourself what the world needs. Ask yourself what makes you come alive, and go do that, because what the world needs is people who have come alive.
+      </blockquote>      
+    </div>
+  )
+}
+
+export default Quote
