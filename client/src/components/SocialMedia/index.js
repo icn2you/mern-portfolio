3376559751dd6a71ce6dcaee03bc543c96ee1a73
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-13 17:58:01 -0700

    feat(components): add social media
    - Add SocialMedia component, which renders social media profiles as well
      as links to email and résumé.
    - Export new component via `index.js` (to be commited subsequently).

---
+++ client/src/components/SocialMedia/index.js
@@ -0,0 +1,51 @@
+/***********************************************************************
+FSWD:  Christopher B. Zenner
+Date:  09/12/2020
+File:  index.js
+Ver.:  0.1.0 20200912
+
+This script contains the SocialMedia React component of my developer 
+portfolio.
+***********************************************************************/
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './style.scss'
+
+const SocialMedia = ({ label, icon, link }) => {
+  const getFAIcon = (faIcon) => {
+    switch (faIcon) 
+    {
+      case 'github':
+      case 'linkedin-in':
+      case 'twitter':
+        return ( 
+          <FontAwesomeIcon 
+            icon={['fab', faIcon]}
+            size="2x"
+            fixedWidth
+            className="align-middle"
+          />
+        )
+      default:
+        return ( 
+          <FontAwesomeIcon 
+            icon={faIcon}
+            size="2x"
+            fixedWidth
+            className="align-middle"
+          />
+        )
+    }
+  }
+
+  return (
+    <div className="social-media align-middle">
+      { getFAIcon(icon) }
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        {label}
+      </a>
+    </div>
+  )
+}
+
+export default SocialMedia
