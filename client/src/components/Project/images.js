fa8e959ff5736013ef57dc867ded3fbc08b293d0
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-21 18:24:50 -0700

    feat(assets): add image import file for build

---
+++ client/src/components/Project/images.js
@@ -0,0 +1,16 @@
+/*
+  Image assets must be required for the build process.
+*/
+require('../../assets/images/sprites/laptop-sprite.png')
+require('../../assets/images/sprites/cookquik-sprite.png')
+require('../../assets/images/sprites/crystals-collector-sprite.png')
+require('../../assets/images/sprites/friendfinder-sprite.png')
+require('../../assets/images/sprites/gif-tastic-sprite.png')
+require('../../assets/images/sprites/google-books-search-sprite.png')
+require('../../assets/images/sprites/lagomify-sprite.png')
+require('../../assets/images/sprites/listenhear-sprite.png')
+require('../../assets/images/sprites/newsscraper-sprite.png')
+require('../../assets/images/sprites/pokeclick-game-sprite.png')
+require('../../assets/images/sprites/rps-multiplayer-sprite.png')
+require('../../assets/images/sprites/woman-trivia-sprite.png')
+require('../../assets/images/sprites/word-guess-sprite.png')
