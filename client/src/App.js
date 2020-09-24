fc3e50b537a0dba609812b3138e2dbd0fd248c5a
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-23 18:41:33 -0700

    feat(app): wrap app in recaptcha provider
    - Require `dotenv` package for access to environment variables.
    - Retrieve reCAPTCHA site key from `.env` file.
    - Wrap App in ReCaptchaProvider component for reCAPTCHA v2 implementation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ReCaptchaProvider } from 'react-recaptcha-x'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faFileWord } from '@fortawesome/free-solid-svg-icons'
 import { Projects } from './pages'
 import './App.scss'
 
+require('dotenv').config()
+
 library.add(fab, faEnvelope, faFileWord)
+
+const RECAPTCHA_V2_SITE_KEY = process.env.REACT_APP_RECAPTCHA_V2_SITE_KEY
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Projects} />
-      </Switch>
-    </Router>
+    <ReCaptchaProvider
+      siteKeyV2={RECAPTCHA_V2_SITE_KEY}
+    >
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Projects} />
+        </Switch>
+      </Router>
+    </ReCaptchaProvider>
   )
 }
 
