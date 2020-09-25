0997ccd95be62b54bd711d7ccda786aff908b222
Christopher Zenner <christopher.zenner@gmail.com>
2020-09-24 23:58:23 -0700

    feat(components): implement recaptcha frontend

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   EReCaptchaV2Size, EReCaptchaV2Theme, ReCaptchaV2
 } from 'react-recaptcha-x'
@@ -6,7 +6,31 @@ import { Button, Form } from 'react-bootstrap'
 import './style.scss'
 
 const ContactForm = () => {
-  const handleReCaptcha = token => {}
+  const [recaptchaV2Token, setRecaptchaV2Token] = useState('')
+  const [recaptchaV2Expired, setRecaptchaV2Expired] = useState(false)
+  const [recaptchaV2Error, setRecaptchaV2Error] = useState(false)
+  const [recaptchaV2Message, setRecaptchaV2Message] = useState(null)
+
+  const handleRecaptchaV2 = token => {
+    if (typeof token === 'string') {
+      setRecaptchaV2Token(token)
+      setRecaptchaV2Expired(false)
+      setRecaptchaV2Error(false)
+    } else if (typeof token === 'boolean' && !token) {
+      setRecaptchaV2Expired(true)
+      setRecaptchaV2Message('Your token has expired. Please check the reCAPTCHA box again.')
+    } else if (token instanceof Error) {
+      setRecaptchaV2Error(true)
+      setRecaptchaV2Message('An error has occured. Please check your connection and try again.')
+    }
+
+    // temporary console output:
+    console.log(`Your token is ${recaptchaV2Token}`)
+    console.log(
+      `Your token has ${(!recaptchaV2Expired) ? 'not ' : null}expired.`)
+    console.log(
+      `An error ${recaptchaV2Error ? 'occurred' : 'did not occur'}.`)
+  }
 
   return (
     <Form>
@@ -59,12 +83,20 @@ const ContactForm = () => {
           value="Reset"
         />
       </Form.Group>
-      <ReCaptchaV2
-        id="recaptcha"
-        callback={handleReCaptcha}
-        size={EReCaptchaV2Size.Normal}
-        theme={EReCaptchaV2Theme.Light}
-      />
+      <Form.Group
+        controlId="formRecaptcha2"
+        className="d-flex justify-content-center pt-4"
+      >
+        <ReCaptchaV2
+          id="recaptcha"
+          callback={handleRecaptchaV2}
+          size={EReCaptchaV2Size.Normal}
+          theme={EReCaptchaV2Theme.Light}
+        />
+        <Form.Text id="recaptcha-msg" muted>
+          { recaptchaV2Message }
+        </Form.Text>
+      </Form.Group>
     </Form>
   )
 }
