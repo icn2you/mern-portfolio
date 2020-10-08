9df361a448b288a9ad755bdd4e2165f38c8a705f
Christopher Zenner <christopher.zenner@gmail.com>
2020-10-07 20:05:26 -0700

    feat(components): validate form input
    [NOTE: At this point, the ContactForm component is solid. It will be ready for production after rigorous unit testing.]
    
    - Add `statusMsg` object to encapsulate all status messages, including
      errors.
    - Add `formValidated` state variable, which serves as a flag to indicate
      when the user's form input has been validated.
    - Add `recaptchaV2MsgAdd` state variable, which serves as a flag to
      indicate when an addendum to the status message should be displayed.
    - Test `recaptchaV2Token` based on its type.
    - Refactor `handleFormSubmit()` method such that it performs the
      following tasks:
      a. Check if the form is valid. If it's not, stop propagation and notify
         user; otherwise hand off the next task to `handleSendMessage()` method.
      b. Check if there is a valid reCAPTCHA v2 token. If not, notify user
         accordingly and return.
      c. Set `formValidated` to true, assuming all other tasks have been
         completed successfully.
    - Require all three form fields (name, email, and message). Use default
      validation—which is not the most robust but will do for now.
    - Set appropriate user feedback for incorrectly completed form fields.
    - Add functionality to display addendum to status message.
    - Tweak styling of status messages.
    
    Tasks
    - [X] Notify user of message send status.
    - [X] Implement basic form validation.
    - [ ] Explore more rigorous form validation in the future.

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -7,6 +7,8 @@ Ver.:  0.1.0 20200826 - basic form
        0.3.0 20200929 - form handling
        0.4.0 20200930 - gatekeeping
        0.5.0 20201002 - message sanitization
+       0.6.0 20201003 - reCAPTCHA v2 reset
+       0.7.0 20201006 - form validation
 
 This script contains the ContactForm React component of my developer portfolio.
 ***********************************************************************/
@@ -29,17 +31,51 @@ const initFormData = Object.freeze({
   message: ''
 })
 
+const statusMsg = {
+  bot: 'Silly, bot! Messages are for humans.',
+  human: 'You appear to be human! You may proceed.',
+  genErr: 'An error occurred. Please attempt to send your message again.',
+  connErr: 'An error has occurred. Please check your connection and try again.',
+  formErr: 'Your message cannot be sent. Please resolve the errors above and attempt to send your message again.',
+  networkErr: 'A network error occurred. Please attempt to verify again.',
+  errAdd: 'If the problem persists, use the email link below to contact me.',
+  contactMade: 'Your message was successfully sent! I\'ll be in touch soon.',
+  tokenExpired: 'Your verification has expired. Please check the reCAPTCHA checkbox again.',
+  tokenNeeded: 'Your message cannot be sent. Please confirm you are human using the reCAPTCHA widget above!'
+}
+
 const ContactForm = () => {
   const [formData, setFormData] = useState(initFormData)
+  const [formValidated, setFormValidated] = useState(false)
   const [recaptchaV2Token, setRecaptchaV2Token] = useState(undefined)
   const [recaptchaV2Exp, setRecaptchaV2Exp] = useState(false)
   const [recaptchaV2Err, setRecaptchaV2Err] = useState(false)
   const [recaptchaV2Msg, setRecaptchaV2Msg] = useState('')
+  const [recaptchaV2MsgAdd, setRecaptchaV2MsgAdd] = useState(false)
 
   useEffect(() => {
-    if (recaptchaV2Token !== '') {
+    if (typeof recaptchaV2Token === 'string') {
+      // ASSERT: We have a token to send to Google's SiteVerify.
       API.isVisitorHuman(recaptchaV2Token)
-        .then(res => console.log(res))
+        .then((res) => {
+          if (res === undefined) {
+            // ASSERT: API returned undefined, which means fetch failed.
+            window.grecaptcha.reset()
+            setRecaptchaV2Token(undefined)
+            setRecaptchaV2Msg(statusMsg.networkErr)
+            setRecaptchaV2MsgAdd(true)
+          } else if (!res.success) {
+            // ASSERT: User appears to be non-human.
+            setRecaptchaV2Token(undefined)
+            setRecaptchaV2Err(true)
+            setRecaptchaV2Msg(statusMsg.bot)
+            setRecaptchaV2MsgAdd(false)
+          } else {
+            // ASSERT: User appears to be human.
+            setRecaptchaV2Msg(statusMsg.human)
+            setRecaptchaV2MsgAdd(false)
+          }
+        })
         .catch(err => console.error(err.stack))
     }
   }, [recaptchaV2Token])
@@ -56,10 +92,27 @@ const ContactForm = () => {
   const handleFormSubmit = ev => {
     ev.preventDefault()
 
-    if (typeof recaptchaV2Token === 'string' &&
-      recaptchaV2Token.length > 0 &&
-      !recaptchaV2Err &&
-      !recaptchaV2Exp) {
+    const form = ev.currentTarget
+
+    if (form.checkValidity() === false) {
+      ev.stopPropagation()
+      setRecaptchaV2Msg(statusMsg.formErr)
+      setRecaptchaV2MsgAdd(false)
+    } else {
+      handleSendMessage()
+    }
+
+    if (typeof recaptchaV2Token !== 'string') {
+      setRecaptchaV2Msg(statusMsg.tokenNeeded)
+      setRecaptchaV2MsgAdd(false)
+      return
+    }
+
+    setFormValidated(true)
+  }
+
+  const handleSendMessage = () => {
+    if (!recaptchaV2Err && !recaptchaV2Exp && formValidated) {
       // Strip any HTML tags from visitor's message.
       formData.message =
         stripHtml(formData.message, {
@@ -68,23 +121,27 @@ const ContactForm = () => {
 
       // Send visitor's message to API for routing.
       API.sendVisitorMsg(formData)
-        .then(res => console.log(res))
+        .then(({ status }) => {
+          if (typeof status === 'boolean' && status) {
+            // Clear form input.
+            setFormData(initFormData)
+
+            /*
+              Reset reCAPTCHA v2 state.
+
+              NOTE: Invoking reset() on the window object's grecaptcha element seems to be the only way to reset the reCAPTCHA v2 element successfully. (See https://stackoverflow.com/questions/46514194/how-to-reset-google-recaptcha-with-react-google-recaptcha/47128103#47128103.) However, I'm not sure this is the best/most elegant solution. I've tried grabbing a ref to the ReCaptchaV2 component with useRef() to no avail. This may be a limitation of the react-recaptcha-x package. (20201003)
+            */
+            setFormValidated(false)
+            window.grecaptcha.reset()
+            setRecaptchaV2Token(undefined)
+            setRecaptchaV2Msg(statusMsg.contactMade)
+            setRecaptchaV2MsgAdd(false)
+          } else {
+            setRecaptchaV2Msg(statusMsg.genErr)
+            setRecaptchaV2MsgAdd(true)
+          }
+        })
         .catch(err => console.error(err.stack))
-
-      // Clear form input.
-      setFormData(initFormData)
-
-      /*
-        Reset reCAPTCHA v2 state, and clear any error message.
-
-        NOTE: Invoking reset() on the window object's grecaptcha element seems to be the only way to reset the reCAPTCHA v2 element successfully. (See https://stackoverflow.com/questions/46514194/how-to-reset-google-recaptcha-with-react-google-recaptcha/47128103#47128103.) However, I'm not sure this is the best/most elegant solution. I've tried grabbing a ref to the ReCaptchaV2 component with useRef() to no avail. This may be a limitation of the react-recaptcha-x package. (20201003)
-      */
-      window.grecaptcha.reset()
-      setRecaptchaV2Token(undefined)
-      setRecaptchaV2Exp(false)
-      setRecaptchaV2Msg('')
-    } else {
-      setRecaptchaV2Msg('Your message cannot be sent. Please confirm you are human using the reCAPTCHA widget above!')
     }
   }
 
@@ -95,34 +152,47 @@ const ContactForm = () => {
       setRecaptchaV2Err(false)
     } else if (typeof token === 'boolean' && !token) {
       setRecaptchaV2Exp(true)
-      setRecaptchaV2Msg('Your verification has expired. Please check the reCAPTCHA checkbox again.')
+      setRecaptchaV2Msg(statusMsg.tokenExpired)
+      setRecaptchaV2MsgAdd(false)
     } else if (token instanceof Error) {
       setRecaptchaV2Err(true)
-      setRecaptchaV2Msg('An error has occurred. Please check your connection and try again.')
+      setRecaptchaV2Msg(statusMsg.connErr)
+      setRecaptchaV2MsgAdd(true)
     }
   }
 
   return (
-    <Form noValidate onSubmit={handleFormSubmit}>
+    <Form noValidate
+      validated={formValidated}
+      onSubmit={handleFormSubmit}
+    >
       <Form.Group controlId="formContactMeName">
         <Form.Label>Name</Form.Label>
         <Form.Control
+          required
           type="text"
           name="name"
           value={formData.name}
           placeholder="Wonder Woman"
           onChange={handleFormInputChg}
         />
+        <Form.Control.Feedback type="invalid">
+          Please enter your name.
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formContactMeEmail">
         <Form.Label>Email</Form.Label>
         <Form.Control
+          required
           type="email"
           name="email"
           value={formData.email}
           placeholder="wonder.woman@example.com"
           onChange={handleFormInputChg}
         />
+        <Form.Control.Feedback type="invalid">
+          Please enter a valid email address.
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Control
         type="hidden"
@@ -137,6 +207,7 @@ const ContactForm = () => {
       <Form.Group controlId="formContactMeMsg">
         <Form.Label>Message</Form.Label>
         <Form.Control
+          required
           as="textarea"
           rows="10"
           name="message"
@@ -144,6 +215,9 @@ const ContactForm = () => {
           placeholder="Talk to me—in plain text, please!"
           onChange={handleFormInputChg}
         />
+        <Form.Control.Feedback type="invalid">
+          Please enter a message.
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group
         controlId="formContactMeBtns"
@@ -178,7 +252,11 @@ const ContactForm = () => {
         className="d-flex justify-content-center"
       >
         <Form.Text id="recaptcha-v2-msg" muted>
-          { recaptchaV2Msg }
+          <div>{ recaptchaV2Msg }</div>
+          { recaptchaV2MsgAdd
+            ? <div>{ statusMsg.errAdd }</div>
+            : ''
+          }
         </Form.Text>
       </Form.Group>
     </Form>
