2c00df550491433967f96782aab2153ea2c2cc9c
Christopher Zenner <christopher.zenner@gmail.com>
2020-10-09 23:27:03 -0700

    refactor(db): add `_cc` and `_subject` fields
    Add `_cc` and `_subject` fields to records, which correspond to hidden form inputs on the frontend, in the ContactForm component.

--- models/message.js
+++ models/message.js
@@ -6,6 +6,8 @@ module.exports = (() => {
   const MessageSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
+    _cc: { type: String, required: true },
+    _subject: { type: String, required: true },
     text: { type: String, required: true } 
   }, { timestamps: true })
 
