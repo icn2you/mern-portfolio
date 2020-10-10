f8100294858d85ee5578f1049cafd29c9ffbca0b
Christopher Zenner <christopher.zenner@gmail.com>
2020-10-09 23:22:51 -0700

    feat(controllers): add messages controller
    The messagess controller has a single function, which is to persist messages from users in the database. Additional methods may be added as necessary in the future.

--- controllers/db/msg-controller.js
+++ controllers/db/msg-controller.js
@@ -1,3 +1,19 @@
 const db = require('../../models')
 
-module.exports = { }
+module.exports = { 
+  create: (req, res) => {
+    const contact = req.body 
+    
+    Object.defineProperty(contact, 'text', { 
+      value: contact.message,
+      enumerable: true 
+    })
+    
+    delete contact['message']
+    
+    db.Message
+      .create(contact)
+      .then(doc => res.json(doc))
+      .catch(err => res.status(422).json(err.stack))
+  }
+}
