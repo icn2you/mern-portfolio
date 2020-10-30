6667e7f87faf300406500a2e611846c6ccdb0449
Christopher Zenner <christopher.zenner@gmail.com>
2020-10-29 23:27:35 -0700

    feat(controllers): prettify user's message
    - Add additional `replyTo` and `html` fields to visitor's message.
    - Create HTML template for message.

--- controllers/mail/mail-controller.js
+++ controllers/mail/mail-controller.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer')
 
-const { GMAIL_USERNAME, GMAIL_PASSWORD } = process.env
+const { GMAIL_ACCTNAME, GMAIL_USERNAME, GMAIL_PASSWORD } = process.env
 
 module.exports = {
   submitContact: (req, res) => {
@@ -17,11 +17,33 @@ module.exports = {
 
     // Format the message.
     const emailMessage = {
-      from: req.body.name,
-      to: GMAIL_USERNAME,
+      from: `'${req.body.name} <${req.body.email}>'`,
+      to: `'${GMAIL_ACCTNAME} <${GMAIL_USERNAME}>'`,
       cc: req.body._cc,
+      replyTo: `'${req.body.name} <${req.body.email}>'`,
       subject: req.body._subject,
-      text: `
+      html:
+        `
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap" rel="stylesheet">
+        <style>
+          body { color: #373838; font-family: 'Nunito'; font-size: 14px; }
+          h2 { color: #84898C; font-size: medium; }
+          a { color: #373838; text-decoration: none; }
+          a:active, a:focus, a:hover { color: #0F4C81; text-decoration: underline; }
+        </style>
+        <div>
+          <h1>Hi, Chris! 🤓</h1>
+          <p>You've got mail from a visitor to your online portfolio:</p>
+          <h2>name:</h2>
+          <p>${req.body.name}</p>
+          <h2>reply-to:</h2>
+          <p>${req.body.email}</p>
+          <h2>message:</h2>
+          <p>${req.body.message}</p>
+        </div>
+        `,
+      text:
+        `
         Hi, Chris.
         A visitor to your online portfolio just sent you the following message:
 
