f8a0f2c342c4e4cd90ce068814221433685b31f1
Christopher Zenner <christopher.zenner@gmail.com>
2020-11-16 21:26:37 -0800

    refactor(api): replace client-side verification
    Replace `isVisitorHuman()` method, which queries Google's site verification API directly from client, with `verifyVisitor()`, which routes the client's verification request to the server via a POST. This modification is necessary so as not to expose secure data via the React front-end.
    
    Task
    - [ ] Replace `isomorphic-fetch` package with `cross-fetch` for consistency.

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,26 +1,15 @@
 require('isomorphic-fetch')
 
-const RECAPTCHA_PROXY_DOMAIN = process.env.REACT_APP_PROXY_DOMAIN ||
-  'https://cors-anywhere.herokuapp.com/'
-const RECAPTCHA_SERVER_KEY = process.env.REACT_APP_REACAPTCH_V2_SERVER_KEY
-
-// DEBUG:
-// console.log(`RECAPTCHA_PROXY_DOMAIN = ${RECAPTCHA_PROXY_DOMAIN}`)
-
 export default {
   getPortfolioProj: filter => fetch(`/db/projects?${filter}`)
     .then(resp => resp.json())
     .catch(err => console.error(err.stack)),
 
-  isVisitorHuman: verificationKey => fetch(
-    `${RECAPTCHA_PROXY_DOMAIN}https://www.google.com/recaptcha/api/siteverify`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-      },
-      body: `secret=${RECAPTCHA_SERVER_KEY}&response=${verificationKey}`
-    })
+  verifyVisitor: token => fetch('/google/verify', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ response: token })
+  })
     .then(resp => resp.json())
     .catch(err => console.error(err.message)),
 
