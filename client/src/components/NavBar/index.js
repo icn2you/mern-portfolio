4ee9702c23a6a254b0482c78490cc41fa1bd6045
Christopher Zenner <christopher.zenner@gmail.com>
2020-11-26 22:45:24 -0800

    refactor(components): display custom navigation
    Display navigation bar dynamically based on the page the user is on.

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -3,6 +3,7 @@ FSWD:  Christopher B. Zenner
 Date:  08/27/2020
 File:  index.js
 Ver.:  0.1.0 20200827
+       0.2.0 20201126
 
 This script contains the NavBar React component of my developer
 portfolio.
@@ -10,14 +11,52 @@ portfolio.
 import React from 'react'
 import './style.scss'
 
+const links = [
+  {
+    label: 'Home',
+    href: '/',
+    pages: 'port'
+  },
+  {
+    label: 'Projects',
+    href: '/#pinned-projects',
+    pages: 'home'
+  },
+  {
+    label: 'Contact',
+    href: '/#contact-me',
+    pages: 'both'
+  },
+  {
+    label: 'About',
+    href: '/#about',
+    pages: 'both'
+  }/*,
+  {
+    label: 'Blog',
+    href: '/blog',
+    pages: 'both'
+  } */
+]
+
 const NavBar = () => {
+  /*
+    NOTE: When the site expands beyond two pages, the following logic
+          will need to be refactored.
+  */
   return (
     <nav>
       <ul>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact-me">Contact</a></li>
-        <li><a href="#about">About</a></li>
-        {/* <li><a href="#">Blog</a></li> */}
+        { window.location.pathname === '/'
+          // Display home page navigation.
+          ? links.filter(link =>
+            link.pages === 'home' || link.pages === 'both').map(link =>
+            <li><a href={link.href}>{link.label}</a></li>)
+          // Display portfolio page naviation.
+          : links.filter(link =>
+            link.pages === 'port' || link.pages === 'both').map(link =>
+            <li><a href={link.href}>{link.label}</a></li>)
+        }
       </ul>
     </nav>
   )
