fe763eddb24a13c31b427d8bd1e6590d44a50e9a
Christopher Zenner <christopher.zenner@gmail.com>
2020-12-01 20:32:01 -0800

    refactor(app): maintain `projects` state variable
    - Promote `projects` state variable from `Projects.js` (the prior
      landing page) to `App.js`.
    - Pass `projects` as props to two pages of site, `Home.js` and
      `Portfolio.js`.
    - Render only highlighted (pinned/starred) projects on `Home.js`.
    - Render all active projects on `Portfolio.js`

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ReCaptchaProvider } from 'react-recaptcha-x'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faFileWord } from '@fortawesome/free-solid-svg-icons'
-import { Projects } from './pages'
+import { Home, Portfolio } from './pages'
+import API from './utils/api'
 import './App.scss'
 
 require('dotenv').config()
@@ -14,13 +15,38 @@ library.add(fab, faEnvelope, faFileWord)
 const RECAPTCHA_V2_SITE_KEY = process.env.REACT_APP_RECAPTCHA_V2_SITE_KEY
 
 const App = () => {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    loadProjects()
+  }, [])
+
+  const loadProjects = () => {
+    API.getPortfolioProj('active=true')
+      .then(res => {
+        // Verify that API did not return an error message from MongoDB.
+        if (typeof res === 'string') {
+          console.error(res)
+        } else {
+          setProjects(res)
+        }
+      })
+      .catch(err => console.error(err.stack))
+  }
+
   return (
     <ReCaptchaProvider
       siteKeyV2={RECAPTCHA_V2_SITE_KEY}
     >
       <Router>
         <Switch>
-          <Route exact path='/' component={Projects} />
+          <Route exact path='/' render={props =>
+            <Home {...props} projects={
+              projects.filter(project => project.starred)}
+            />}
+          />
+          <Route exact path='/portfolio' render={props =>
+            <Portfolio {...props} projects={projects} />} />
         </Switch>
       </Router>
     </ReCaptchaProvider>
